dfcf8b75112835c750c9976b86add1476ce4497d
cudaman73 <dale.evans.3@gmail.com>
2022-05-22 15:10:35 -0500

    Initializes webdev bootcamp repository. Adds tindog, basic personal site, and dicee project

---
+++ dicee-challenge-js/index.js
@@ -0,0 +1,22 @@
+function randNum() {
+  return Math.round(Math.random() * 6)
+}
+
+function playGame() {
+  var dice1 = randNum()
+  var dice2 = randNum()
+  document.querySelector(".img1").src = "images/dice" + dice1 + ".png"
+  document.querySelector(".img2").src = "images/dice" + dice2 + ".png"
+
+  if (dice1 > dice2) {
+    document.querySelector("h1").innerHTML = "🏲 Player 1 Wins!"
+  }
+  else if (dice2 > dice1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🏲"
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!"
+  }
+}
+
+playGame()
