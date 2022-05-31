fc2089337ba91dd0a7a7d0252654c4bd6a3db467
cudaman73 <dale.evans.3@gmail.com>
2022-05-31 16:18:15 -0500

    Adds initial game functions, version 1.0 playable

--- 15 - simon-says-game/index.js
+++ 15 - simon-says-game/index.js
@@ -4,8 +4,10 @@ let userClickedPattern = [];
 let round = 0
 
 function nextSequence() {
+  userClickedPattern = []
   //select next color semi-randomly and store it
   randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
+  console.log("adding " + randomChosenColor + " to the game pattern")
   gamePattern.push(randomChosenColor);
   //animation to let user know which color was selected
   $("#" + randomChosenColor).fadeOut(75).fadeIn(75);
@@ -26,20 +28,44 @@ function buttonAnimation(key) {
   }, 100)
 }
 
+function checkAnswer(round) {
+  if (gamePattern[round] === userClickedPattern[round]) {
+    console.log(round + " success")
+    if (userClickedPattern.length == gamePattern.length) {
+      setTimeout(function() {
+        nextSequence()
+      }, 1000)
+    }
+  } else {
+      $("h1").text("Game Over, press any key to restart");
+      $("body").addClass("game-over");
+      setTimeout(function() {
+        $("body").removeClass("game-over")
+      }, 200);
+      gameOver();
+  }
+}
+
+function gameOver() {
+  gamePattern = [];
+  round = 0
+}
+
 $(".btn").click(function(event) {
   //grab the id of the button clicked and store it
   buttonAnimation(event.target.id)
   let userChosenColor = event.target.id;
   userClickedPattern.push(userChosenColor)
   playSound(userChosenColor)
-  if (userClickedPattern[round] != gamePattern[round]) {
-    $("body").css("background-color", "red")
+  //check to see if the clicked answer is correct
+  if (checkAnswer(userClickedPattern.length - 1)) {
+
   }
-  nextSequence();
 })
 
 $(document).keypress(function() {
-  if (round == 0) {
+  if (round === 0) {
     nextSequence()
+    round++
   }
 })
